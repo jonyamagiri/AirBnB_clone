0735a4d3c58246d728291c0ed5dacdf0e0b7d515
Joab Nyamagiri <jonyamagiri@gmail.com>
2022-10-29 15:22:15 +0300

    created BaseModel class

---
+++ models/base_model.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Module base_model.py defines all common attributes/methods for other classes
+"""
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    """Defines all common attributes/methods for other classes
+    Methods:
+        def __init__(self)
+        def __str__(self)
+        def save(self)
+        def to_dict(self)
+    """
+    def __init__(self):
+        """Initializes the class"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """Prints instance as: [<class name>] (<self.id>) <self.__dict__>"""
+        objs = '[{}] ({}) {}'
+        return objs.format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        """Updates the attribute updated_at with the current datetime"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Returns a dictionary containing all keys/values of the instance"""
+        temp_dict = self.__dict__.copy()
+        temp_dict["created_at"] = self.created_at.isoformat()
+        temp_dict["updated_at"] = self.updated_at.isoformat()
+        temp_dict["__class__"] = self.__class__.__name__
+        return temp_dict
